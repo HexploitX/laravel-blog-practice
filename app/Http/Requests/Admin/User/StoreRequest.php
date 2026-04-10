859e65d18a367c2240cf31224f0a8b30e72c78e3
Alexandr <sasha51315121@mail.ru>
2026-04-10 17:02:28 +0300

    feat: implement User CRUD with roles and password hashing

---
+++ app/Http/Requests/Admin/User/StoreRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Admin\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string',
+            'role' => 'required|integer',
+        ];
+    }
+
+    public function messages() {
+        return [
+            'name.required' => 'Это поле необходимо заполнить',
+            'email.required' => 'Это поле необходимо заполнить',
+            'email.email' => 'Почта должна быть валидной и содержать @',
+            'email.unique' => 'Пользователь с такой почтой уже существует',
+            'password.required' => 'Это поле необходимо заполнить'
+        ];
+    }
+}
